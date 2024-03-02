3d3ecbb811bbf388b7cee8bcfbad20a50ab97486
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-03 00:50:18 +0100

    en cour...

--- Login Form/ContentView.swift
+++ Login Form/ContentView.swift
@@ -8,16 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var identifiant = ""
+    @State private var motDePasse = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        
+        VStack(alignment:.center){
+            
+
+            TextField("Identifiant", text: $identifiant )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+                .multilineTextAlignment(.leading)
+                .padding(.leading,-15)
+
+            
+                .padding()
+            
+            TextField("Mot de Passe", text: $motDePasse )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                .multilineTextAlignment(.leading)
+
+
+            
+            
+            
+            
+            
+            Text("connexion")
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
+                .bold()
+            
+            
+            
+            
+        } // fin vstack
         .padding()
-    }
-}
+        
+        
+        
+        
+        
+        
+        
+        
+    } // FIN BODY
+} // FIN STRUCT
 
 #Preview {
     ContentView()
