dfe84514b2dc3b96439b462079c366ba93415355
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-02 23:56:02 +0100

    Initial Commit

---
+++ Login Form/Login_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_FormApp.swift
+//  Login Form
+//
+//  Created by  Ixart on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Login_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
