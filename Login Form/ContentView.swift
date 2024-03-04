a2c84a541fdaa5a3590cd1b391156dd22d97d678
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-04 02:08:45 +0100

    fin

--- Login Form/ContentView.swift
+++ Login Form/ContentView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 let userNameWin = "Allan"  // nom pour la bonne conection
-let password = "Azerty"    // mot de passe pour la bonne conexion
+let password = "AA"    // mot de passe pour la bonne conexion
+
 
 struct ContentView: View {
     @State private var identifiant = ""
     @State private var motDePasse = ""
+    @State private var message = "" // message d'avertisement
+    @State private var isConnected = Bool() // pour changer un text (apparence)
+    @State private var showMessage = false
 
     var body: some View {             
-        
         VStack(alignment:.center){
             
             Text("vous n'avez pas entrer votre login")
                 .foregroundStyle(.red)
                 .bold()
             
-
             TextField("Identifiant", text: $identifiant )
             
                 .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -31,8 +33,6 @@ struct ContentView: View {
 
                 .multilineTextAlignment(.leading)
                 .padding(.leading,-15)
-
-            
                 
             
             TextField("Mot de Passe", text: $motDePasse )
@@ -44,43 +44,45 @@ struct ContentView: View {
                 .multilineTextAlignment(.leading)
                 .padding(.leading,-15)
 
+            
+            Button(action: {if identifiant == userNameWin && motDePasse == password {
+                message = "connecté ✅"
+                
+                isConnected = true // il est vrai donc vert voir en bas
+                showMessage = false // affiche pas
 
+            } else {
+                message = "Vous n'avez Pas Entré Votre Login Try Again⚠️."
+                
+                isConnected = false // il est faux donc tout le contre air
+                showMessage = true // Affiche le message d'avertissement
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                           showMessage = true
+                       }
 
-            
-            
-            
-            
-            Button(action: {
-                
-                
-                
-            }, label: {
+            }
+        }, label: {
                 
                 Text("connexion")
                     .font(.system(size: 30))
                     .multilineTextAlignment(.center)
                     .bold()
+                    .padding(70)
             })
             
-            
-            
-            
-            
-            
-            
+            Text(message)
+                .foregroundColor(isConnected ? .green : .red) // pour prendre en compte le vert sinon rouge
+                .font(.system(size: isConnected ? 30 : 35)) // pareil pour prendre en compte la taile sinon taille  35
+                .padding(50)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .bold()
         } // fin vstack
         .padding()
-        
-        
-        
-        
-        
-        
-        
-        
+
     } // FIN BODY
 } // FIN STRUCT
-
 #Preview {
     ContentView()
 }
